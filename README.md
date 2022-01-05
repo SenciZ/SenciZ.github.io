e73bcf28471d526751c2bdef45066e690f929610
Senad Zenkovic <zenkovic88@gmail.com>
2022-01-05 09:36:30 -0700

    upload created page

---
+++ README.md
@@ -0,0 +1,5 @@
+# PortfolioPage
+
+Create my personal portfolio page in order to improve my current skills and to showcase my projects to potential employers.
+
+https://senciz.github.io/PortfolioPage/
---
+++ index.js
@@ -0,0 +1,23 @@
+let seeWorkBtn = document.getElementById("workBtn")
+seeWorkBtn.addEventListener("click", function(e){
+    let position = document.querySelector(".workPage").offsetTop;
+    window.scrollTo(0, position,);
+})
+
+let aboutMeBtn = document.getElementById("aboutBtn")
+aboutMeBtn.addEventListener("click", function(e){
+    let position = document.querySelector(".aboutPage").offsetTop;
+    window.scrollTo(0, position,);
+})
+
+let techBtn = document.getElementById("techBtn")
+techBtn.addEventListener("click", function(e){
+    let position = document.querySelector(".techPage").offsetTop;
+    window.scrollTo(0, position,);
+})
+
+let contactBtn = document.getElementById("contactBtn")
+contactBtn.addEventListener("click", function(e){
+    let position = document.querySelector(".contactPage").offsetTop;
+    window.scrollTo(0, position,);
+})
