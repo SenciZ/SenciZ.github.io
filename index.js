2df1f6207deac108a057c1e85b7958fa92d46a48
Senad Zenkovic <zenkovic88@gmail.com>
2022-02-01 13:49:34 -0700

    add semicolons

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
-const aboutLink = document.getElementById("paboutLink")
-const skillsLink = document.getElementById("skillsLink")
-const homeLink = document.getElementById("homeLink")
-const contactLink = document.getElementById("contactLink")
-const hamburgerMenu =document.getElementById('hamburgerMenu')
+const aboutLink = document.getElementById("paboutLink");
+const skillsLink = document.getElementById("skillsLink");
+const homeLink = document.getElementById("homeLink");
+const contactLink = document.getElementById("contactLink");
+const hamburgerMenu = document.getElementById("hamburgerMenu");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
-    document.getElementById('hamburger').innerHTML='menu'
+    document.getElementById("hamburger").innerHTML = "menu";
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
     menu.classList.add("showMenu");
-    document.getElementById('hamburger').innerHTML='close'
+    document.getElementById("hamburger").innerHTML = "close";
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
   }
@@ -25,12 +25,10 @@ function toggleMenu() {
 
 hamburger.addEventListener("click", toggleMenu);
 
-hamburgerMenu.addEventListener('click', ()=>{
-        menu.classList.remove("showMenu");
-        document.getElementById('hamburger').innerHTML='menu'
+hamburgerMenu.addEventListener("click", () => {
+  menu.classList.remove("showMenu");
+  document.getElementById("hamburger").innerHTML = "menu";
 
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-})
-
-
+  closeIcon.style.display = "none";
+  menuIcon.style.display = "block";
+});
